4ff59622d2eb1c7ee8e0bec43b8730ba6bc67611
Deepa Pandey <deepa.ultracures@gmail.com>
2020-10-02 02:43:35 +0530

    opinion given -> JS

---
+++ Javascript/answer.js
@@ -0,0 +1,11 @@
+
+
+function myFunction(){
+    document.getElementById("response").innerHTML = "Nopes buddy 😝! It's a Markup language...Why? Generally, a programming language allows you to describe some sort of process of doing something, whereas HTML is a way of adding context and structure to text.🤪"
+}
+
+function noFunction(){
+    document.getElementById("response").innerHTML = "Ohhooo great😎! It's a markup language instead."
+}
+
+
